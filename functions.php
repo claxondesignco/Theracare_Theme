4680264fadc1307970cadfdd025b9b5d04321952
Adam Claxon <hi@adamclaxon.com>
2017-04-25 18:29:41 -0400

    Start of child theme

---
+++ functions.php
@@ -0,0 +1,68 @@
+<?php
+	
+	/*
+	*
+	*	Uplift Functions - Child Theme
+	*	------------------------------------------------
+	*	These functions will override the parent theme
+	*	functions. We have provided some examples below.
+	*
+	*
+	*/
+	
+	/* LOAD PARENT THEME STYLES
+	================================================== */
+	function uplift_child_enqueue_styles() {
+	    wp_enqueue_style( 'uplift-parent-style', get_template_directory_uri() . '/style.css' );
+	
+	}
+	add_action( 'wp_enqueue_scripts', 'uplift_child_enqueue_styles' );
+	
+	
+	/* LOAD THEME LANGUAGE
+	================================================== */
+	/*
+	*	You can uncomment the line below to include your own translations
+	*	into your child theme, simply create a "language" folder and add your po/mo files
+	*/
+	
+	// load_theme_textdomain('swiftframework', get_stylesheet_directory().'/language');
+	
+	
+	/* REMOVE PAGE BUILDER ASSETS
+	================================================== */
+	/*
+	*	You can uncomment the line below to remove selected assets from the page builder
+	*/
+	
+	// function spb_remove_assets( $pb_assets ) {
+	//     unset($pb_assets['parallax']);
+	//     return $pb_assets;
+	// }
+	// add_filter( 'spb_assets_filter', 'spb_remove_assets' );	
+
+
+	/* ADD/EDIT PAGE BUILDER TEMPLATES
+	================================================== */
+	function custom_prebuilt_templates($prebuilt_templates) {
+			
+		/*
+		*	You can uncomment the lines below to add custom templates
+		*/
+		// $prebuilt_templates["custom"] = array(
+		// 	'id' => "custom",
+		// 	'name' => 'Custom',
+		// 	'code' => 'your-code-here'
+		// );
+
+		/*
+		*	You can uncomment the lines below to remove default templates
+		*/
+		// unset($prebuilt_templates['home-1']);
+		// unset($prebuilt_templates['home-2']);
+
+		// return templates array
+	    return $prebuilt_templates;
+
+	}
+	//add_filter( 'spb_prebuilt_templates', 'custom_prebuilt_templates' );
